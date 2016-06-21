ff4d71c425653a088b98e81fe95206af6e7cb63c
min <side112358@gmail.com>
2016-06-21 16:20:52 +0800

    完成基礎認證及使用者建置

---
+++ database/seeds/CreateUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CreateUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+        	'name' => '主管理者',
+        	'email' => 'admin@admin.com',
+        	'job_title' => '系統管理者',
+        	'phone' => '0963-383-891',
+        	'password' => bcrypt('123456')
+        	]);
+    }
+}
